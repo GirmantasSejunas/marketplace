0b1e66b3319468acdf6f5aa4aa0ac935078ce755
Girmantas <girmantas123@gmail.com>
2022-11-02 09:57:46 +0200

    added home components

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Clothes from '../components/homeComponents/Clothes'
+import Devices from '../components/homeComponents/Devices'
+import RealEstate from '../components/homeComponents/RealEstate'
+import Transport from '../components/homeComponents/Transport'
 
 const Home = () => {
   return (
 
 
     <div className='home'>
+      < RealEstate />
+      < Transport />
+      < Devices />
+      < Clothes />
+
     </div>
 
   )
